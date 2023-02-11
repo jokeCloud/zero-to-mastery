e4c7ccf59c64ab543784aa6a72c466c0ad425959
johansen masterson <medalhaemerson@gmail.com>
2023-02-11 16:16:34 -0300

    exericeses

---
+++ dev-python/p1_python-basics/p01_data_types.py
@@ -0,0 +1,27 @@
+# Fundamental Data Types
+a = int()
+b = float()
+c = bool()
+d = str()
+e = list()
+f = tuple()
+g = set()
+h = dict()
+
+print(
+    type(a),
+    type(b),
+    type(c),
+    type(d),
+    type(e),
+    type(f),
+    type(g),
+    type(h),
+)
+
+
+# Classes -> Custom Types
+
+# Specialized Data Types
+
+# None
---
+++ dev-python/p1_python-basics/p02_data_types_pratice.py
@@ -0,0 +1,17 @@
+# Fundamental Data Types
+print(f'{type(6)} | {2 + 4}')
+print(f'{type(-2)} | {2 - 4}')
+print(f'{type(8)} | {2 * 4}')
+print(f'{type(0.5)} | {2 / 4}')
+
+print()
+print(f'{type(10.56)} | {20*1.1}')
+print()
+
+print(2 ** 3)
+
+print(400 // 20)
+print(20 ** 2)
+print(20 * 20)
+
+print(10 % 3)
